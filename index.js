5e501ce1a94ef7c877164da33e775645483b50a8
kennedy <kennedy.mwai@student.moringaschool.com>
2025-03-14 20:35:49 +0300

    completed quiz

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+function superbowlWin(record) {
+
+    const winningRecord = record.find(entry => entry.result === "W");
+    
+    return winningRecord ? winningRecord.year : undefined;
+}
+
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+  ];
+
+  console.log(superbowlWin(record)); 
+
+  const noWinsRecord = [
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" }
+  ];
+
+  console.log(superbowlWin(noWinsRecord));
+
+
+
